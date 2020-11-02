a2a2a2455a9e250e098d4298e901ccd0428d213b
bhaasitadesiraju <bhaasitadesiraju@gmail.com>
2020-11-02 16:31:16 -0500

    Adding Peer process and Write log classes

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -4,4 +4,10 @@ public class Constants {
   public static final String HANDSHAKE_HEADER = "P2PFILESHARINGPROJ";
   public static final String ZERO_BITS = "0000000000";
   public static final int INVALID_PEER_ID = -1;
+  public static final String COMMON_CONFIG_FILE = "main/resources/config/Common.cfg";
+  public static final String PEER_CONFIG_FILE = "main/resources/config/PeerInfo.cfg";
+  public static final String LOCALHOST = "localhost";
+  public static final int FIRST_PEER_PORT_NUMBER = 6008;
+  public static final int HAS_FILE = 1;
+  public static final int NO_FILE = 0;
 }
